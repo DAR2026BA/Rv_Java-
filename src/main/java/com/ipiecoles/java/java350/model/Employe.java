3dbd88adb7dea3cc09952db83d95e0768c6c7ee2
Cheikhouna-Bamba1 <163699169+Cheikhouna-Bamba1@users.noreply.github.com>
2026-03-25 11:29:09 +0000

    Rename variable 'var' to 'value' in getNbRtt method

--- src/main/java/com/ipiecoles/java/java350/model/Employe.java
+++ src/main/java/com/ipiecoles/java/java350/model/Employe.java
@@ -60,16 +60,16 @@ public class Employe {
 
     public Integer getNbRtt(LocalDate d){
         int i1 = d.isLeapYear() ? 365 : 366;
-        int var = 104;
+        int value = 104;
         switch (LocalDate.of(d.getYear(),1,1).getDayOfWeek()){
-            case THURSDAY: if(d.isLeapYear()) var =  var + 1; break;
-            case FRIDAY: if(d.isLeapYear()) var =  var + 2; else var =  var + 1; break;
-            case SATURDAY: var = var + 1; break;
+            case THURSDAY: if(d.isLeapYear()) value =  value + 1; break;
+            case FRIDAY: if(d.isLeapYear()) value =  value + 2; else value =  value + 1; break;
+            case SATURDAY: value = value + 1; break;
             default:
                 break;
         }
         int monInt = (int) Entreprise.joursFeries(d).stream().filter(localDate -> localDate.getDayOfWeek().getValue() <= DayOfWeek.FRIDAY.getValue()).count();
-        return (int) Math.ceil((i1 - Entreprise.NB_JOURS_MAX_FORFAIT - var - Entreprise.NB_CONGES_BASE - monInt) * tempsPartiel);
+        return (int) Math.ceil((i1 - Entreprise.NB_JOURS_MAX_FORFAIT - value - Entreprise.NB_CONGES_BASE - monInt) * tempsPartiel);
     }
 
     /**
